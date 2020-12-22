9ef1ec5f5093193b3b92c5a35dea318925f993cb
Stefan Pranger <stefan.pranger@student.tugraz.at>
2020-12-22 16:07:02 +0100

    added opt dir override bitvector to multiplier
    This is mainly used by the SMG model checker to override row group
    optimization directions.

--- src/storm/environment/solver/MultiplierEnvironment.h
+++ src/storm/environment/solver/MultiplierEnvironment.h
@@ -1,23 +1,31 @@
 #pragma once
 
+#include <boost/optional.hpp>
+
 #include "storm/environment/solver/SolverEnvironment.h"
 #include "storm/solver/SolverSelectionOptions.h"
 
+#include "storm/storage/BitVector.h"
+
 namespace storm {
-    
+
     class MultiplierEnvironment {
     public:
-        
+
         MultiplierEnvironment();
         ~MultiplierEnvironment();
-        
+
         storm::solver::MultiplierType const& getType() const;
         bool const& isTypeSetFromDefault() const;
         void setType(storm::solver::MultiplierType value, bool isSetFromDefault = false);
-        
+
+        void setOptimizationDirectionOverride(storm::storage::BitVector optimizationDirectionOverride);
+        boost::optional<storm::storage::BitVector> const& getOptimizationDirectionOverride() const;
+
     private:
         storm::solver::MultiplierType type;
         bool typeSetFromDefault;
+
+        boost::optional<storm::storage::BitVector> optimizationDirectionOverride = boost::none;
     };
 }
-
