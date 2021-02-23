c9d5e2959a2a834dcdb0bd8cd18f67d72c6d871e
Stefan Pranger <stefan.pranger@student.tugraz.at>
2021-02-23 17:48:22 +0100

    let MDP helper return type store choiceValues

--- src/storm/modelchecker/prctl/helper/MDPModelCheckingHelperReturnType.h
+++ src/storm/modelchecker/prctl/helper/MDPModelCheckingHelperReturnType.h
@@ -9,31 +9,34 @@ namespace storm {
     namespace storage {
         class BitVector;
     }
-    
+
     namespace modelchecker {
         namespace helper {
             template<typename ValueType>
             struct MDPSparseModelCheckingHelperReturnType {
-                
+
                 MDPSparseModelCheckingHelperReturnType(MDPSparseModelCheckingHelperReturnType const&) = delete;
                 MDPSparseModelCheckingHelperReturnType(MDPSparseModelCheckingHelperReturnType&&) = default;
-                
+
                 MDPSparseModelCheckingHelperReturnType(std::vector<ValueType>&& values, std::unique_ptr<storm::storage::Scheduler<ValueType>>&& scheduler = nullptr) : values(std::move(values)), scheduler(std::move(scheduler)) {
                     // Intentionally left empty.
                 }
-                
+
                 virtual ~MDPSparseModelCheckingHelperReturnType() {
                     // Intentionally left empty.
                 }
-                
+
                 // The values computed for the states.
                 std::vector<ValueType> values;
-                
+
+                // The values computed for the available choices.
+                std::vector<ValueType> choiceValues;
+
                 // A scheduler, if it was computed.
                 std::unique_ptr<storm::storage::Scheduler<ValueType>> scheduler;
             };
         }
-        
+
     }
 }
 
