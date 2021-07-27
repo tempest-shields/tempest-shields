f92b2104b599a4a2bd6b16f41cbda529d0b6c05a
hannah <hannah.mertens@rwth-aachen.de>
2021-07-27 11:07:10 +0200

    TODOs and fixed an error during scheduler creation

--- src/storm/modelchecker/prctl/SparseDtmcPrctlModelChecker.cpp
+++ src/storm/modelchecker/prctl/SparseDtmcPrctlModelChecker.cpp
@@ -132,7 +132,7 @@ namespace storm {
                     << *da->getAcceptance()->getAcceptanceExpression() << " as acceptance condition.");
 
             const SparseDtmcModelType& dtmc = this->getModel();
-            storm::modelchecker::helper::SparseLTLHelper<ValueType, false> helper(dtmc.getTransitionMatrix(), dtmc.getNumberOfStates());
+            storm::modelchecker::helper::SparseLTLHelper<ValueType, false> helper(dtmc.getTransitionMatrix());
             storm::modelchecker::helper::setInformationFromCheckTaskDeterministic(helper, checkTask, dtmc);
 
             // Compute Satisfaction sets for APs
@@ -164,7 +164,7 @@ namespace storm {
                 modelDot.close();
             }
 
-            storm::modelchecker::helper::SparseLTLHelper<ValueType, false> helper(dtmc.getTransitionMatrix(), dtmc.getNumberOfStates());
+            storm::modelchecker::helper::SparseLTLHelper<ValueType, false> helper(dtmc.getTransitionMatrix());
             storm::modelchecker::helper::setInformationFromCheckTaskDeterministic(helper, checkTask, dtmc);
 
             // Compute Satisfaction sets for APs
